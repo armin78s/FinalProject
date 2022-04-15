5f3edcb168e3bc6c3d00f448af0fd74196baf5ea
armin78s <armin.saadatmand@gmail.com>
2022-04-15 10:27:08 +0430

    create cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect } from "react";
 import Nav from "./components/Nav";
 import Form from "./components/Form";
 import Products from "./components/Products";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategories } from "./store/category-action";
 import { fetchProducts } from "./store/product-action";
-import {loading, products } from "./store";
+import { loading, products } from "./store";
 import Loading from "./components/Loading";
+import Carts from "./components/Cart";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   const product = useSelector(products);
@@ -18,16 +20,16 @@ function App() {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
-
 
   console.log(product);
 
   return (
     <Fragment>
+      
+
       <Nav />
       <div className="p-4">
         <Form />
